2d4ec2896cd3bc807dd84787c5d716c8d479c5e4
Jesper Kristensen <cableman@linuxdev.dk>
2023-06-29 14:34:08 +0200

    EV-244: Added first take on Event entity

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -2,19 +2,135 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\Blameable;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ApiResource]
 class Event
 {
+    use TimestampableEntity;
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $excerpt = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 32, nullable: true)]
+    private string $langcode = 'und';
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $ticket_url = null;
+
+    #[ORM\Column]
+    private bool $public = true;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getExcerpt(): ?string
+    {
+        return $this->excerpt;
+    }
+
+    public function setExcerpt(?string $excerpt): static
+    {
+        $this->excerpt = $excerpt;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getLangcode(): ?string
+    {
+        return $this->langcode;
+    }
+
+    public function setLangcode(?string $langcode): static
+    {
+        $this->langcode = $langcode;
+
+        return $this;
+    }
+
+    public function getTicketUrl(): ?string
+    {
+        return $this->ticket_url;
+    }
+
+    public function setTicketUrl(?string $ticket_url): static
+    {
+        $this->ticket_url = $ticket_url;
+
+        return $this;
+    }
+
+    public function isPublic(): ?bool
+    {
+        return $this->public;
+    }
+
+    public function setPublic(bool $public): static
+    {
+        $this->public = $public;
+
+        return $this;
     }
 }
