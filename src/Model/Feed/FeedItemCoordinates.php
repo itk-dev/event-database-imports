e86d067074e4d5d6e0406ee2e90af286bb01c64a
Jesper Kristensen <cableman@linuxdev.dk>
2023-09-05 15:05:39 +0200

    EV-262: Updated field names in FeedItemCoordinates

--- src/Model/Feed/FeedItemCoordinates.php
+++ src/Model/Feed/FeedItemCoordinates.php
@@ -8,15 +8,10 @@ final class FeedItemCoordinates
      * Default constructor.
      *
      * The coordinates are stings because the feeds use different separators and notation for numbers in the input.
-     *
-     * @param string $lat
-     *  Latitude
-     * @param string $long
-     *   Longitude
      */
     public function __construct(
-        public string $lat = '',
-        public string $long = '',
+        public string $latitude = '',
+        public string $longitude = '',
     ) {
     }
 }
