c6d641bcb8b43da54208df99f8974d76fa7649f9
Jesper Kristensen <cableman@linuxdev.dk>
2023-09-20 22:24:06 +0200

    EV-266: Start on image processing

---
+++ tests/ImageServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Services\Image;
+use App\Services\TagsNormalizer;
+use App\Tests\Utils\PhpUnitUtils;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+#[CoversClass(Image::class)]
+final class ImageServiceTest extends KernelTestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testCreatePath()
+    {
+        $depth = 2;
+        $size = 10;
+        $path = PhpUnitUtils::callPrivateMethod(
+            $this->getImageService(),
+            'generatePath',
+            ['https://bora-bora.dk/wp-content/uploads/2023/06/DansBabyDans-Web-Main-2600x1500px-scaled.jpg', false, $depth, $size]
+        );
+
+        $this->assertEquals($depth*$size+2, strlen($path));
+        $this->assertStringEndsWith('/', $path);
+    }
+
+    /**
+     * Helper function to bootstrap the service tested.
+     *
+     * @return Image
+     *   The service
+     *
+     * @throws \Exception
+     */
+    private function getImageService(): Image
+    {
+        self::bootKernel();
+        $container = ImageServiceTest::getContainer();
+
+        return $container->get(Image::class);
+    }
+}
