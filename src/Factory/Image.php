637d46276747eb8180e01355f7aaa204280e35b3
Jesper Kristensen <cableman@linuxdev.dk>
2023-09-20 22:31:00 +0200

    EV-266: Image processing code style fixes

--- src/Factory/Image.php
+++ src/Factory/Image.php
@@ -2,16 +2,16 @@
 
 namespace App\Factory;
 
+use App\Entity\Image as ImageEntity;
 use App\Repository\ImageRepository;
-use \App\Entity\Image as ImageEntity;
 
 class Image
 {
     public function __construct(
         private readonly ImageRepository $imageRepository,
         private readonly \App\Services\Image $image
-    )
-    {}
+    ) {
+    }
 
     public function createOrUpdate(string $url, ?ImageEntity $image): ImageEntity
     {
@@ -23,10 +23,8 @@ class Image
             $image->setSource($url);
         } else {
             // Check if image have been updated.
-
         }
 
         return $image;
-
     }
 }
--- tests/ImageServiceTest.php
+++ tests/ImageServiceTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use App\Services\Image;
-use App\Services\TagsNormalizer;
 use App\Tests\Utils\PhpUnitUtils;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -26,7 +25,7 @@ final class ImageServiceTest extends KernelTestCase
             ['https://bora-bora.dk/wp-content/uploads/2023/06/DansBabyDans-Web-Main-2600x1500px-scaled.jpg', false, $depth, $size]
         );
 
-        $this->assertEquals($depth*$size+2, strlen($path));
+        $this->assertEquals($depth * $size + 2, strlen($path));
         $this->assertStringEndsWith('/', $path);
     }
 
