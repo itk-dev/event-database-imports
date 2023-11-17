8b4b187a9c3eecb83423d659f282a289d0993c5f
Jesper Kristensen <cableman@linuxdev.dk>
2023-11-17 12:25:12 +0100

    EV-318: Added very basic CURD for admin UI

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Event;
+use App\Entity\Feed;
+use App\Entity\Location;
+use App\Entity\Occurrence;
+use App\Entity\Organization;
+use App\Entity\Tag;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +21,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(EventCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -35,12 +43,18 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App');
+            ->setTitle('Event database');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Feeds', 'fa fa-rss', Feed::class);
+        yield MenuItem::linkToCrud('Tags', 'fa fa-tags', Tag::class);
+        yield MenuItem::linkToCrud('Location', 'fa fa-location-dot', Location::class);
+        yield MenuItem::linkToCrud('Events', 'fa fa-calendar', Event::class);
+        yield MenuItem::linkToCrud('Organizer', 'fa fa-sitemap', Organization::class);
+        yield MenuItem::linkToCrud('Occurrences', 'fa fa-repeat', Occurrence::class);
     }
 }
