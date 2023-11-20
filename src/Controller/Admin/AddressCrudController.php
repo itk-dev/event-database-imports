2b442e581ee525b71d81f9667f74c75f9ab5b6ac
Jesper Kristensen <cableman@linuxdev.dk>
2023-11-20 13:37:54 +0100

    EV-318: Added address entity crud

--- src/Controller/Admin/AddressCrudController.php
+++ src/Controller/Admin/AddressCrudController.php
@@ -2,29 +2,17 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Event;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Locale;
+use App\Entity\Address;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
-class EventCrudController extends AbstractCrudController
+class AddressCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Event::class;
-    }
-
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            // first argument = datetime pattern or date format; second optional argument = time format
-//            ->setDateTimeFormat('...', '...')
-//            ->setTimezone(Locale::getDenmark())
-
-            ->setNumberFormat('%.2d');
+        return Address::class;
     }
 
     /*
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Address;
 use App\Entity\Event;
 use App\Entity\Feed;
 use App\Entity\Location;
@@ -21,23 +22,9 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        //return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
         return $this->redirect($adminUrlGenerator->setController(EventCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -49,12 +36,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Events', 'fa fa-calendar', Event::class);
+        yield MenuItem::linkToCrud('Occurrences', 'fa fa-repeat', Occurrence::class);
+        yield MenuItem::linkToCrud('Location', 'fa fa-location-dot', Location::class);
+        yield MenuItem::linkToCrud('Address', 'fa fa-address-book', Address::class);
         yield MenuItem::linkToCrud('Feeds', 'fa fa-rss', Feed::class);
         yield MenuItem::linkToCrud('Tags', 'fa fa-tags', Tag::class);
-        yield MenuItem::linkToCrud('Location', 'fa fa-location-dot', Location::class);
-        yield MenuItem::linkToCrud('Events', 'fa fa-calendar', Event::class);
         yield MenuItem::linkToCrud('Organizer', 'fa fa-sitemap', Organization::class);
-        yield MenuItem::linkToCrud('Occurrences', 'fa fa-repeat', Occurrence::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
     }
 }
