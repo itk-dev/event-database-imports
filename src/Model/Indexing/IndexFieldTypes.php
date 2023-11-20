a9ccb8b1223da78022656c4363bdb11ac7556e06
Jesper Kristensen <cableman@linuxdev.dk>
2023-11-20 13:07:37 +0100

    EV-268: Update date format comment
    Co-authored-by: Mikkel Ricky <rimi@aarhus.dk>

--- src/Model/Indexing/IndexFieldTypes.php
+++ src/Model/Indexing/IndexFieldTypes.php
@@ -8,7 +8,7 @@ final class IndexFieldTypes
     public const DATEFORMAT = 'c';
 
     // One would think one could use the named ISO formats (but, NO) and Elastic say they use the format in the link.
-    // Here it also needs to be "z" and not "Z" to match the ISO format.
+    // Here it also needs to be "z" (“time-zone name”) and not "Z" (“zone-offset”) to match the ISO format.
     // @see https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
     public const DATEFORMAT_ES = "yyyy-MM-dd'T'HH:mm:ssz";
 }
