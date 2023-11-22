15de1b3d881192f9f8dbff28c0f6eae5c0e33c00
Jesper Kristensen <cableman@linuxdev.dk>
2023-11-22 14:36:56 +0100

    EV-318: Very basic easy admin for user

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +19,26 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+            IdField::new('id')
+                ->setDisabled()
+                ->hideWhenCreating(),
+
+            TextField::new('name'),
+            EmailField::new('mail'),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->hideOnIndex(),
+            BooleanField::new('enabled'),
+
+            FormField::addFieldset('Edited')
+                ->hideWhenCreating(),
+            DateTimeField::new('updated_at')
+                ->setLabel('Last updated')
+                ->setDisabled()
+                ->hideWhenCreating()
+                ->setFormat(DashboardController::DATETIME_FORMAT),        ];
     }
-    */
 }
