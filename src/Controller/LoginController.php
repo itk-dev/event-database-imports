3ede74ff7a925348547a90bdad9cf7cb31745d75
Jesper Kristensen <cableman@linuxdev.dk>
2023-11-23 13:38:37 +0100

    EV-318: Refactored login form

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class LoginController extends AbstractController
 {
@@ -15,9 +16,23 @@ class LoginController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
             'error' => $error,
+            'last_username' => $lastUsername,
+            'page_title' => 'Event database log in',
+            'csrf_token_intention' => 'authenticate',
+            'target_path' => $this->generateUrl('admin'),
+            'username_label' => new TranslatableMessage('login.user.mail'),
+            'password_label' => new TranslatableMessage('login.user.password'),
+            'sign_in_label' => new TranslatableMessage('login.label'),
+            // @TODO: build forgot password form.
+            // 'forgot_password_enabled' => true,
+            // 'forgot_password_path' => $this->generateUrl('...', ['...' => '...']),
+            // 'forgot_password_label' => 'Forgot your password?',
+            'remember_me_enabled' => true,
+            'remember_me_checked' => true,
+            'remember_me_label' => new TranslatableMessage('login.page.remember'),
         ]);
     }
 
