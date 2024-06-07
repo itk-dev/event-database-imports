2575b73da0075ac84d965df57e41d3b4c76448aa
turegjorup <ture@ignatz.dk>
2024-06-07 15:31:34 +0200

    1601: Ensure all timestamps are persisted as UTC in the database

---
+++ src/Doctrine/Extensions/DBAL/Types/UTCDateTimeImmutableType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Doctrine\Extensions\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
+
+class UTCDateTimeImmutableType extends DateTimeImmutableType
+{
+    private static \DateTimeZone $utc;
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if ($value instanceof \DateTimeImmutable) {
+            if (self::getUtc()->getName() !== $value->getTimezone()->getName()) {
+                $mutable = \DateTime::createFromImmutable($value);
+                $mutable->setTimezone(self::getUtc());
+
+                $value = \DateTimeImmutable::createFromMutable($mutable);
+            }
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?\DateTimeImmutable
+    {
+        if (null === $value || $value instanceof \DateTimeImmutable) {
+            return $value;
+        }
+
+        $converted = \DateTimeImmutable::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::getUtc()
+        );
+
+        if (false === $converted) {
+            throw ConversionException::conversionFailedFormat($value, $this->getName(), $platform->getDateTimeFormatString());
+        }
+
+        return $converted;
+    }
+
+    private static function getUtc(): \DateTimeZone
+    {
+        return self::$utc ??= new \DateTimeZone('UTC');
+    }
+}
