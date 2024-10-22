1cf9bf43c8b25f212f9543c1e24f867a88d17223
turegjorup <ture@ignatz.dk>
2024-10-22 10:19:00 +0200

    2690: Added profiler to dev setup

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
