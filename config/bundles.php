3e64b2a413c72a25cfe3ab1d729297399ed8b79f
turegjorup <ture@ignatz.dk>
2024-11-26 23:33:13 +0100

    3096: Add missing bundle to config/bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
