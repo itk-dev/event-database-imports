12cbe344733d8706f7a3817f456421157770505a
turegjorup <ture@ignatz.dk>
2024-12-04 12:11:00 +0100

    2432: Expand event delete command to work by feed

--- src/Command/EventDeleteCommand.php
+++ src/Command/EventDeleteCommand.php
@@ -3,21 +3,26 @@
 namespace App\Command;
 
 use App\Repository\EventRepository;
+use App\Repository\FeedRepository;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:event:delete',
-    description: 'Delete an event. CAn be used to clean-up feed events as these cannot be deleted through EasyAdmin',
+    description: 'Delete event(s). Can be used to clean-up feed events as these cannot be deleted through EasyAdmin',
 )]
 class EventDeleteCommand extends Command
 {
     public function __construct(
         private readonly EventRepository $eventRepository,
+        private readonly FeedRepository $feedRepository,
+        private CacheManager $imageCacheManager,
     ) {
         parent::__construct();
     }
@@ -25,42 +30,105 @@ class EventDeleteCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('event-id', InputArgument::REQUIRED, 'ID of the event to delete')
+            ->addOption('event-id', null, InputOption::VALUE_REQUIRED, 'ID of the event to delete')
+            ->addOption('feed-id', null, InputOption::VALUE_REQUIRED, 'ID of the feed to delete all events from')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $eventId = $input->getArgument('event-id');
+        $eventId = $input->getOption('event-id');
+        $feedId = $input->getOption('feed-id');
 
-        $event = $this->eventRepository->find($eventId);
+        if (null !== $eventId && null !== $feedId) {
+            $io->warning('Event ID and Feed ID are not allowed simultaneously');
 
-        if (!$event) {
-            $io->error(sprintf('Event with ID %s not found', $eventId));
-
-            return Command::FAILURE;
+            return Command::INVALID;
         }
 
-        $eventTitle = $event->getTitle() ?? 'UNKNOWN TITLE';
-        $question = sprintf('Delete event with ID %s: %s?', $eventId, $eventTitle);
+        if (null === $eventId && null === $feedId) {
+            $io->warning('You must use either the Event ID or Feed ID option');
 
-        if ($io->confirm($question, false)) {
-            try {
-                $this->eventRepository->remove($event, true);
+            return Command::INVALID;
+        }
 
-                $io->success(sprintf('Event with ID %s: %s deleted', $eventId, $eventTitle));
+        if (null !== $eventId) {
+            $event = $this->eventRepository->find($eventId);
 
-                return Command::SUCCESS;
-            } catch (\Throwable $e) {
-                $io->error(sprintf('Error deleting event with ID %s: %s', $eventId, $e->getMessage()));
+            if (!$event) {
+                $io->error(sprintf('Event with ID %s not found', $eventId));
 
                 return Command::FAILURE;
             }
-        } else {
-            $io->info('Event not deleted');
 
-            return Command::INVALID;
+            $eventTitle = $event->getTitle() ?? 'UNKNOWN TITLE';
+            $question = sprintf('Delete event with ID %s: %s?', $eventId, $eventTitle);
+
+            if ($io->confirm($question, false)) {
+                try {
+                    $this->eventRepository->remove($event, true);
+
+                    $io->success(sprintf('Event with ID %s: %s deleted', $eventId, $eventTitle));
+
+                    return Command::SUCCESS;
+                } catch (\Throwable $e) {
+                    $io->error(sprintf('Error deleting event with ID %s: %s', $eventId, $e->getMessage()));
+
+                    return Command::FAILURE;
+                }
+            } else {
+                $io->info('Event not deleted');
+
+                return Command::INVALID;
+            }
+        }
+
+        if (null !== $feedId) {
+            $feed = $this->feedRepository->find($feedId);
+            if (!$feed) {
+                $io->error(sprintf('Feed with ID %s not found', $feedId));
+
+                return Command::FAILURE;
+            }
+
+            $name = $feed->getName() ?? 'UNKNOWN';
+            $question = sprintf('Delete ALL events from feed  %s: %s?', $feedId, $name);
+
+            if ($io->confirm($question, false)) {
+                try {
+                    $events = $feed->getEvents();
+                    $count = count($events);
+
+                    $progressBar = new ProgressBar($output, $count);
+                    $progressBar->start();
+                    $progressBar->setMessage(sprintf('Deleting events from feed  %s: %s?', $feedId, $name));
+                    $progressBar->display();
+
+                    foreach ($events as $event) {
+                        $image = $event->getImage()?->getLocal();
+                        if (null !== $image) {
+                            $this->imageCacheManager->remove($image);
+                        }
+
+                        $this->eventRepository->remove($event, true);
+
+                        $progressBar->advance();
+                    }
+
+                    $progressBar->finish();
+
+                    return Command::SUCCESS;
+                } catch (\Throwable $e) {
+                    $io->error(sprintf('Error deleting events with ID %s: %s', $feedId, $e->getMessage()));
+
+                    return Command::FAILURE;
+                }
+            } else {
+                $io->info('Events not deleted');
+
+                return Command::INVALID;
+            }
         }
     }
 }
