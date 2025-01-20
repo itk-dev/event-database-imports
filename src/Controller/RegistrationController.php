a29793df580854c7045962730b6a47d0eba402d2
turegjorup <ture@ignatz.dk>
2025-01-20 13:45:22 +0100

    Enable pretty urls for easy admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,7 @@ use App\Types\UserRoles;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
@@ -20,7 +21,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 #[Route('/admin/register')]
-class RegistrationController extends AbstractDashboardController
+class RegistrationController extends AbstractController
 {
     public function __construct(
         private readonly EmailVerifier $emailVerifier,
