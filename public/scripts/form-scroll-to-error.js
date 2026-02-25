f1db3c66be8505a8adf75b123b66e477dc3f34a4
Ture Gjørup <ture@ignatz.dk>
2026-02-25 20:31:40 +0100

    Update public/scripts/form-scroll-to-error.js
    Co-authored-by: Mikkel Ricky <rimi@aarhus.dk>

--- public/scripts/form-scroll-to-error.js
+++ public/scripts/form-scroll-to-error.js
@@ -1,6 +1,6 @@
 document.addEventListener('ea.form.error', function (event) {
     const form = event.detail.form;
-    const firstInvalid = form.querySelector('input:invalid:not(:disabled), select:invalid:not(:disabled), textarea:invalid:not(:disabled)');
+    const firstInvalid = form.querySelector(':invalid:not(:disabled)');
 
     if (firstInvalid) {
         firstInvalid.scrollIntoView({ behavior: 'smooth', block: 'center' });
