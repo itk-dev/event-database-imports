51a8c8f96c52db87b691f7efbb6c631b1242ad65
turegjorup <ture@ignatz.dk>
2026-03-09 10:38:57 +0100

    5110: Increase length of allowed URLs on Event entity

---
+++ migrations/Version20260309091234.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260309091234 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event CHANGE url url VARCHAR(2048) DEFAULT NULL, CHANGE ticket_url ticket_url VARCHAR(2048) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event CHANGE url url VARCHAR(255) DEFAULT NULL, CHANGE ticket_url ticket_url VARCHAR(255) DEFAULT NULL');
+    }
+}
